d2484d5e32e5588be320025f5a2a91cfee0a8f69
KFlowerz <k.flores@live.com.mx>
2017-04-19 01:16:27 -0500

    main
    main

---
+++ MasterDetailsBinding/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MasterDetailsBinding
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new LeagueList();
+            this.lvMain.ItemsSource = ViewModel;
+        }
+        public LeagueList ViewModel { get; set; }
+    }
+}
